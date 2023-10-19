bbc530b94c6999b64194078fe646ea3550fa1ccf
VishalP-Bacancy <vishal.pansuriya@bacancy.com>
2023-10-19 12:17:53 +0530

    added authorization header in cors

--- apps/pinq-capital/src/main.ts
+++ apps/pinq-capital/src/main.ts
@@ -1,35 +1,35 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { GlobalExceptionsFilter } from './filter/global-exception.filter';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ConfigService } from '@nestjs/config';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { GlobalExceptionsFilter } from "./filter/global-exception.filter";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  const port = configService.get<number>('PORT') || 8080;
+  const port = configService.get<number>("PORT") || 8080;
 
   app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: '*',
+    allowedHeaders: ["content-type", "Authorization"],
+    origin: "*",
     credentials: true,
   });
 
-  app.setGlobalPrefix('v1/api');
+  app.setGlobalPrefix("v1/api");
 
   // Register the global exception filter
   app.useGlobalFilters(new GlobalExceptionsFilter());
 
   const config = new DocumentBuilder()
-    .setTitle('PINQ Capital')
-    .setDescription('Platform for unsecured loans')
-    .setVersion('1.0')
+    .setTitle("PINQ Capital")
+    .setDescription("Platform for unsecured loans")
+    .setVersion("1.0")
     // .addTag('Bacancy') // Use the same tag as in @ApiTags
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  SwaggerModule.setup("swagger", app, document);
 
   await app.listen(port, () => {
     console.log(`Server started at Port: ${port}`);
